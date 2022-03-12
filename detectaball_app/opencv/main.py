fe272620e9609fb98608ed24b630970b946e708d
Breeze1124 <andy5552555@gmail.com>
2022-03-12 17:10:08 +0800

    與球拍、桌子碰撞

--- detectaball_app/opencv/main.py
+++ detectaball_app/opencv/main.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
+global l_dx
+global l_dy
 
+list = []
 
 def empty(a):
     pass
@@ -17,18 +20,30 @@ def draw_direction(img, lx, ly, nx, ny):
         r = (dx**2 + dy**2)**0.5
         dx = int(dx/r*40)
         dy = int(dy/r*40)
-        # print(dx, dy)
+        print(dx, dy)
+    
+    if(len(list) != 0):
+        if((list[0] > 0 and dy < 0)):
+            cv2.rectangle(img, (nx, ny), (nx+20, ny+20), (255, 255, 0), 2)
+    if(len(list) != 0):
+        list.remove(list[0])
+    list.append(dy)
+    #if(dx !=  or dy != l_dy):
+     #   cv2.rectangle(img, (nx, ny), (nx+2, ny+2), (0, 255, 0), 2)
+        
+    #l_dx = dx
+    #l_dy = dy
     cv2.arrowedLine(img, (60, 100), (60+dx, 100+dy), (0, 255, 0), 2)
 
 
 
 frameWidth = 640
 frameHeight = 480
-cap = cv2.VideoCapture("Resources/OM.mp4")
+cap = cv2.VideoCapture("C:\\Users\\Administration\\Desktop\\test\\table5.mp4")
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 80)
-pulse_ms = 30
+pulse_ms = 300
 
 
 # cv2.namedWindow("HSV")
@@ -40,8 +55,8 @@ pulse_ms = 30
 # cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
 # cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
-lower = np.array([4, 180, 156])
-upper = np.array([32, 255, 255])
+lower = np.array([4, 180, 156])     # 适用于橙色乒乓球4<=h<=32
+upper = np.array([32, 255, 255])    #色相、飽和度、亮度
 
 targetPos_x = 0
 targetPos_y = 0
@@ -74,7 +89,7 @@ while True:
     for cnt in contours:
         area = cv2.contourArea(cnt)
         # print(area)
-        if area > 50:
+        if area > 0:
             x, y, w, h = cv2.boundingRect(cnt)
             lastPos_x = targetPos_x
             lastPos_y = targetPos_y
@@ -83,9 +98,9 @@ while True:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.circle(img, (targetPos_x, targetPos_y), 2, (0, 255, 0), 4)
 
-
+   
     cv2.putText(img, "({:0<2d}, {:0<2d})".format(targetPos_x, targetPos_y), (20, 30),
-                cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+                cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2) 
     draw_direction(img, lastPos_x, lastPos_y, targetPos_x, targetPos_y)
 
     imgStack = np.hstack([img, imgOutput])
