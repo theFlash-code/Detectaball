112fc430389eb76e8e0cac7d62d7c38fb3b033fe
SL510457 <88879304+SL510457@users.noreply.github.com>
2022-04-07 20:52:11 +0800

    Firebase problem not solved

--- detectaball_app/lib/main.dart
+++ detectaball_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Bpage.dart';
 import 'start.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
